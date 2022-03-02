0ae800c54bb0f3300c6470e9722ce1cdf2b5cdfe
YingTong <kehuasun@qq.com>
2022-03-02 20:21:35 +0800

    Data structure: Binary tree

--- DataStructureAndAlgorithm/src/BinaryTrees.java
+++ DataStructureAndAlgorithm/src/BinaryTrees.java
@@ -1,9 +1,50 @@
-class BinaryTrees {
+import java.util.ArrayList;
+import java.util.List;
 
+class BinaryTrees { // 二叉树
+    private TreeNode root; // rootnode
 
+    public BinaryTrees() {
+    }
+
+    public BinaryTrees(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public List<Integer> preOrderTraversal() { // 前序遍历封装方法
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            root.preOrderTraversal(list);
+        }
+        return list;
+    }
+
+    public List<Integer> infixOrderTraversal() { // 前序遍历封装方法
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            root.infixOrderTraversal(list);
+        }
+        return list;
+    }
+
+    public List<Integer> suffixOrderTraversal() { // 前序遍历封装方法
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            root.suffixOrderTraversal(list);
+        }
+        return list;
+    }
 }
 
-class TreeNode {
+class TreeNode { // 二叉树节点
     private int val;
     private TreeNode left;
     private TreeNode right;
@@ -43,5 +84,35 @@ class TreeNode {
 
     public void setRight(TreeNode right) {
         this.right = right;
+    }
+
+    public void preOrderTraversal(List<Integer> list) { // 前序遍历核心程序
+        list.add(val);
+        if (left != null) {
+            left.preOrderTraversal(list);
+        }
+        if (right != null) {
+            right.preOrderTraversal(list);
+        }
+    }
+
+    public void infixOrderTraversal(List<Integer> list) { // 前序遍历核心程序
+        if (left != null) {
+            left.infixOrderTraversal(list);
+        }
+        list.add(val);
+        if (right != null) {
+            right.infixOrderTraversal(list);
+        }
+    }
+
+    public void suffixOrderTraversal(List<Integer> list) { // 前序遍历核心程序
+        if (left != null) {
+            left.suffixOrderTraversal(list);
+        }
+        if (right != null) {
+            right.suffixOrderTraversal(list);
+        }
+        list.add(val);
     }
 }
